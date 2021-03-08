a57b6eb51e96e50a1d1bd3afc9165c21703cf62f
kiing78 <kev97250@hotmail.fr>
2021-03-08 17:02:32 +0100

    version 1.1

--- enregistrement.php
+++ enregistrement.php
@@ -1,6 +1,6 @@
 <?php
-      
-if (isset($_POST['submit']))
+      // si le bouton "valid" est envoyé
+if (isset($_POST['valid']))
 {
    /* on test si les champ sont bien remplis */
     if(!empty($_POST['Nom']) and !empty($_POST['Prenom']) and !empty($_POST['Email']) and !empty($_POST['password']) and !empty($_POST['repeatpassword']))
@@ -18,14 +18,18 @@ if (isset($_POST['submit']))
                 try {
                     $pdo=new PDO("mysql:host=127.0.0.1;dbname=achatenligne","root",""); 
                     echo "<br>Connexion BDD OK";
-                   
+                  
+                    //prepare la requete INSERT INTO et on la met dans la variable $req
                     $req = $pdo->prepare("INSERT INTO connexion (Nom,Prenom,Email,password) VALUES (?,?,?,?)");
-                    
+                    /*on prend les champs rempli du formulaire et
+                    on execute la requete qui est stocké dans la variable $req */
                     $req->execute (array($_POST["Nom"],$_POST["Prenom"],$_POST["Email"],$_POST["password"]));
                     echo "<br>Donnée entrée dans la BDD ";
                 
                     }   
+                    //on attrape l'erreur qu'on stock dans la variable $e
                 catch(PDOException $e) { 
+                    //la variable $e utilise la methode "getMessage"
                     echo $e->getMessage();
 
                                         }                         
