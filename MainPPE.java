a37b96367d250557939b94ccfe271201d0cbb94a
kiing78 <kev97250@hotmail.fr>
2021-03-26 12:40:04 +0100

    ajout des requete et nouvelle bddAcces

--- MainPPE.java
+++ MainPPE.java
@@ -1,10 +1,28 @@
 package PPE;
 
+import javax.swing.JFrame;
+
 public class MainPPE {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PanelPPE fenetre = new PanelPPE();
+		PPE fenetre = new PPE();
+		
+		BddAccess bdd=new BddAccess();
+		// Charger le driver
+		bdd.chargerDriver();
+		
+		// Se connecter a la base de donnees
+		bdd.bddConnect("achatenligne");
+		
+		// Creation de la requete
+		bdd.creerRequete();
+		
+		fenetre.setRefBdd(bdd);
+		bdd.setRefPPE(fenetre);
+		
+	
+		
 
 	}
 
