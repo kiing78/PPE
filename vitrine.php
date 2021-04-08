7cae7ceecec23be2102d48eac5986dc0ef6597ea
mohamed8892 <39552805+mohamed8892@users.noreply.github.com>
2021-04-08 14:56:31 +0200

    Add files via upload

--- vitrine.php
+++ vitrine.php
@@ -35,29 +35,47 @@ if(isset($_POST['Submit']))
 }
 echo'</table>';
 
-            } if($BtonConsultationAchat == 'achat' and  is_numeric($Quantite))
+            } if($BtonConsultationAchat == 'achat' and  is_numeric($Quantite) and $Quantite > 0 and !empty($Quantite))
                      {      
-
-                            echo 'Vous avez commande : <br>';
-                            echo '<br>';
-                            echo'<table><tr><td>Genre</td><td>TypeVetement</td><td>Taille</td><td>Couleur</td><td>Prix</td></tr>';
-                            echo '<tr><td>'.$genre.'</td><td>'.$TypeVetement.'</td><td>'.$Taille.'</td><td>'.$Couleur.'</td><td>'.$Quantite.'</td></tr>';
-
-
-                        $qte = $pdo->prepare("SELECT QuantiteDispo FROM stock");
-
-                        $req2 = $pdo->prepare("UPDATE stock(QuantiteDispo) VALUE () ");
-
-
-
+                        $req=$pdo->prepare("SELECT  QuantiteDispo,TypeVet  FROM stock WHERE TypeVet='$TypeVetement' and Taille = '$Taille' and Couleur='$Couleur' and Genre='$genre'");
+                        $req->execute();// execute la requete
+                      
+            
+                        // Si la requete a un nombre de tour égal à 0 alors la personne n'existe pas et est ensuite envoyé vers la page *enregistrer.html*
+                        if($req->rowCount()==0)
+                        { 
+                            //si l'article demander existe pas
+                            echo " l'article demander n'existe pas ";
+                        }
+                        else
+                        {  
+                               
+                            //recupere les valeur de la colonne de la variable requete(ici qte) et la stock dans $result
+                            //mais vu qu'on a preciser la condition de la requete typeVet alors c'est juste la valeur de 
+                            $result = $req->fetchColumn(0);
+                            //verification si la quantité est inferieur ou egal a 0
+                            if($result <= 0){
+                                echo "cette article n'a plus de stock";
+                            }
+                            else{
+                            //soustraction de la  valeur et stock dans une nouvelle variable
+                            $Quantite = $result-$Quantite;
+                            //requete update qui permet de mettre la nouvelle quantité soustraite dans le typeVetement selectionner
+                            $req1=$pdo->prepare("UPDATE stock SET QuantiteDispo='$Quantite' WHERE TypeVet='$TypeVetement' and Taille = '$Taille' and Couleur='$Couleur' and Genre='$genre'");
+                            $req1->execute();
+                            print_r("quantité=$Quantite");
+                            }
+                            
+                         
+                        }
                         
-                      } 
-                        
 
-        }else echo "Veuillez selectionnez tous les champs";
+        }else {
+                echo "Veuillez saisir tous les champs ";
+        }
         
    }
-
+}
 
 
 
